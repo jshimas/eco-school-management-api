b389d56970868735b658cbd9442f1ace43bcb1e9
Justinas Simas <simjustinas@gmail.com>
2023-06-12 16:03:23 +0100

    logging equality of dates

--- models/activity.js
+++ models/activity.js
@@ -36,7 +36,8 @@ module.exports = (sequelize, DataTypes) => {
             console.log(new Date(value));
             console.log(this.startDate);
             console.log(new Date(this.startDate));
-            if (new Date(value) === new Date(this.startDate)) return; // In DB yyyy-mm-dd hh:mm:ss, but care only about yyyy-dd-mm part
+            console.log(new Date(value) == new Date(this.startDate));
+            if (new Date(value) == new Date(this.startDate)) return; // In DB yyyy-mm-dd hh:mm:ss, but care only about yyyy-dd-mm part
             if (new Date(value) < new Date())
               throw new Error("Date should be today or in the future");
           },
